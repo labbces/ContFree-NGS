3055700d765308f281a6bbfa7dbb03f643a8df5c
Felipe Vaz Peres <felipe.vzps@gmail.com>
2021-07-07 17:15:40 -0300

    Update ContFree-NGS.py

--- ContFree-NGS.py
+++ ContFree-NGS.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-'''						Description
+'''				Description
+	Removing reads from contaminating organisms in NGS datasets
 '''
 
 import argparse
@@ -11,35 +12,69 @@ from Bio import SeqIO
 # Creating arguments
 parser = argparse.ArgumentParser(prog='ContFree-NGS.py', description='Removing reads from contaminating organisms in Next Generation Sequencing datatasets', add_help=True)
 parser.add_argument('--taxonomy', dest='taxonomy_file', metavar='<taxonomy file>', help='A taxonomy classification file', required=True)
-parser.add_argument('--left', dest='left_file', metavar='<left file>', help='left read file', required=True)
-parser.add_argument('--right', dest='right_file', metavar='<right file>', help='right read file', required=True)
+parser.add_argument('--sequencing_type, --s', dest='sequencing_type', metavar='<p or s>', help='paired-end (p) or single-end (s)', required=True)
+parser.add_argument('--R1', dest='R1_file', metavar='<R1 file>', help='read file 1', required=True)
+parser.add_argument('--R2', dest='R2_file', metavar='<R2 file>', help='read file 2', required=False)
 parser.add_argument('--taxon', dest='taxon', metavar='<Taxon>', type=str, help='Only this taxon and its descendants will be maintained',required=True)
 parser.add_argument('--v', '--version', action='version', version='%(prog)s v1.0')
 
 #Getting arguments
 args = parser.parse_args()
 taxonomy_file = args.taxonomy_file
-left_file = args.left_file
-right_file = args.right_file
+sequencing_type = args.sequencing_type
+R1_file = args.R1_file
+R2_file = args.R2_file
 taxon = args.taxon
+paired = 0
+single = 0
+
+if sequencing_type == 'p':
+	paired = 1
+	print("Working with paired-end reads \n")
+elif sequencing_type == 's':
+	single = 1
+	print("Working with single-end reads \n")
+else: 
+	print("ERROR: --sequencing_type must be 'p' for paired-end reads or 's' for single-end reads")
+	print("Exiting. \n")
+	exit()
 
 #Checking for indexed file and create it if dont exists	
-def check_indexed_fastq_files():
-	indexdb_left_file = left_file[:-5] + "index"
-	indexdb_right_file = right_file[:-5] + "index"
-	if not os.path.exists(indexdb_left_file) and not os.path.exists(indexdb_right_file):
-		print("Indexing fastq files, please wait ... \n")
-		left_index_db = SeqIO.index_db(indexdb_left_file, left_file, "fastq")
-		right_index_db = SeqIO.index_db(indexdb_right_file, right_file, "fastq")
+def create_indexed_db_for_paired_fastq_files():
+	indexdb_R1_file = R1_file[:-5] + "index"
+	indexdb_R2_file = R2_file[:-5] + "index"
+	if not os.path.exists(indexdb_R1_file) and not os.path.exists(indexdb_R2_file):
+		print("Creating an indexed database for your paired-end reads, please wait ... \n")
+		R1_index_db = SeqIO.index_db(indexdb_R1_file, R1_file, "fastq")
+		R2_index_db = SeqIO.index_db(indexdb_R2_file, R2_file, "fastq")
 
-check_indexed_fastq_files()
+def create_indexed_db_for_single_fastq_files():
+	indexdb_R1_file = R1_file[:-5] + "index"
+	if not os.path.exists(indexdb_R1_file):
+		print("Creating an indexed database for your single-end reads, please wait ... \n")
+		R1_index_db = SeqIO.index_db(indexdb_R1_file, R1_file, "fastq")
+
+if paired == 1:
+	create_indexed_db_for_paired_fastq_files()
+else: 
+	create_indexed_db_for_single_fastq_files()
 
 #Opening indexed files
-try:
-	index_left = SeqIO.index_db(left_file[:-5] + "index")
-	index_right = SeqIO.index_db(right_file[:-5] + "index")
-except: 
-	print("An exception occurred")
+if paired == 1:
+	try:
+		index_R1 = SeqIO.index_db(R1_file[:-5] + "index")
+		index_R2 = SeqIO.index_db(R2_file[:-5] + "index")
+	except: 
+		print("ERROR: An exception occurred while trying to open your indexed paired-end databse")
+		print("Exiting. \n")
+		exit()
+else:
+	try:
+		index_R1 = SeqIO.index_db(R1_file[:-5] + "index")
+	except: 
+		print("ERROR: An exception occurred while trying to open your indexed single-end databse")
+		print("Exiting. \n")
+		exit()
 
 #Getting NCBI taxonomy database and user taxon
 ncbi = NCBITaxa()
@@ -52,45 +87,82 @@ user_tax_id = list(the_values)[0][0]
 descendants.append(user_tax_id)
 
 #Create output names
-filtered_left = left_file[:-5] + "filtered.fastq"
-filtered_right = right_file[:-5] + "filtered.fastq"
-unfiltered_left = left_file[:-5] + "unclassified.fastq"
-unfiltered_right = right_file[:-5] + "unclassified.fastq"
+if paired == 1:
+	filtered_R1 = R1_file[:-5] + "filtered.fastq"
+	filtered_R2 = R2_file[:-5] + "filtered.fastq"
+	unfiltered_R1 = R1_file[:-5] + "unclassified.fastq"
+	unfiltered_R2 = R2_file[:-5] + "unclassified.fastq"
+else: 
+        filtered_R1 = R1_file[:-5] + "filtered.fastq"
+        unfiltered_R1 = R1_file[:-5] + "unclassified.fastq"
 
 #Create counter
 count_descendants_sequences = 0
 count_unclassified_sequences = 0
 count_contaminant_sequences = 0 
+
 #Filtering files
-with open(taxonomy_file, "r") as taxonomy_classification_file, open(filtered_left, "w") as classified_left, open(filtered_right, "w") as classified_right, open(unfiltered_left, "w") as unclassified_left, open(unfiltered_right, "w") as unclassified_right:
-	for line in taxonomy_classification_file:
+if paired == 1:
+	with open(taxonomy_file, "r") as taxonomy_classification_file, open(filtered_R1, "w") as classified_R1, open(filtered_R2, "w") as classified_R2, open(unfiltered_R1, "w") as unclassified_R1, open(unfiltered_R2, "w") as unclassified_R2:
+		for line in taxonomy_classification_file:
 
-		#Getting IDs
-		left_sequence_id = line.split()[1] + "/1"
-		right_sequence_id = line.split()[1] + "/2"
-		taxonomy_id = int(line.split()[2])
+			#Getting IDs
+			R1_sequence_id = line.split()[1] + "/1"
+			R2_sequence_id = line.split()[1] + "/2"
+			taxonomy_id = int(line.split()[2])
 
-		#Getting sequences in descendants (user taxonomic level)
-		if line.startswith("C"):
-			if taxonomy_id in descendants:
-				count_descendants_sequences += 1
-				SeqIO.write(index_left[left_sequence_id], classified_left, "fastq")
-				SeqIO.write(index_right[right_sequence_id], classified_right, "fastq")
-			else:  
-				count_contaminant_sequences += 1
-		#Getting unclassified reads in taxonomy_classification_file file
-		elif line.startswith("U"):
-			count_unclassified_sequences += 1
-			SeqIO.write(index_left[left_sequence_id], unclassified_left, "fastq")
-			SeqIO.write(index_right[right_sequence_id], unclassified_right, "fastq")
+			#Getting sequences in descendants (user taxonomic level)
+			if line.startswith("C"):
+				if taxonomy_id in descendants:
+					count_descendants_sequences += 1
+					SeqIO.write(index_R1[R1_sequence_id], classified_R1, "fastq")
+					SeqIO.write(index_R2[R2_sequence_id], classified_R2, "fastq")
+				else:  
+					count_contaminant_sequences += 1
+			#Getting unclassified reads in taxonomy_classification_file file
+			elif line.startswith("U"):
+				count_unclassified_sequences += 1
+				SeqIO.write(index_R1[R1_sequence_id], unclassified_R1, "fastq")
+				SeqIO.write(index_R2[R2_sequence_id], unclassified_R2, "fastq")
 
-	print("-------------------------------------------------")
-	print("Contamination removal was successfully completed!")
-	print("-------------------------------------------------")
-	print("{} descendants sequences: {}".format(taxon, count_descendants_sequences))
-	print("Contaminant sequences: {}".format(count_contaminant_sequences))
-	print("Unlabelled sequences: {}".format(count_unclassified_sequences))
-	print("-------------------------------------------------")
-	print("{} descendants sequences are in the filtered files".format(taxon))
-	print("Contaminant sequences were discarded")
-	print("Unlabelled sequences are in the unclassified files")
+		print("-------------------------------------------------")
+		print("Contamination removal was successfully completed!")
+		print("-------------------------------------------------")
+		print("{} descendants sequences: {}".format(taxon, count_descendants_sequences))
+		print("Contaminant sequences: {}".format(count_contaminant_sequences))
+		print("Unlabelled sequences: {}".format(count_unclassified_sequences))
+		print("-------------------------------------------------")
+		print("{} descendants sequences are in the filtered files".format(taxon))
+		print("Contaminant sequences were discarded")
+		print("Unlabelled sequences are in the unclassified files")
+
+else:
+	with open(taxonomy_file, "r") as taxonomy_classification_file, open(filtered_R1, "w") as classified_R1, open(unfiltered_R1, "w") as unclassified_R1:
+		for line in taxonomy_classification_file:
+
+			#Getting IDs
+			R1_sequence_id = line.split()[1]
+			taxonomy_id = int(line.split()[2])
+
+			#Getting sequences in descendants (user taxonomic level)
+			if line.startswith("C"):
+				if taxonomy_id in descendants:
+					count_descendants_sequences += 1
+					SeqIO.write(index_R1[R1_sequence_id], classified_R1, "fastq")
+				else:  
+					count_contaminant_sequences += 1
+			#Getting unclassified reads in taxonomy_classification_file file
+			elif line.startswith("U"):
+				count_unclassified_sequences += 1
+				SeqIO.write(index_R1[R1_sequence_id], unclassified_R1, "fastq")
+
+		print("-------------------------------------------------")
+		print("Contamination removal was successfully completed!")
+		print("-------------------------------------------------")
+		print("{} descendants sequences: {}".format(taxon, count_descendants_sequences))
+		print("Contaminant sequences: {}".format(count_contaminant_sequences))
+		print("Unlabelled sequences: {}".format(count_unclassified_sequences))
+		print("-------------------------------------------------")
+		print("{} descendants sequences are in the filtered files".format(taxon))
+		print("Contaminant sequences were discarded")
+		print("Unlabelled sequences are in the unclassified files")
